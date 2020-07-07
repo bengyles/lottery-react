61bbf965b66cacef6c700f3944a4bd04da928eeb
bengyles <bengyles@hotmail.com>
2020-07-07 19:00:21 +0200

    Pick a winner

--- src/App.js
+++ src/App.js
@@ -32,6 +32,18 @@ class App extends Component {
     this.setState({message: 'You have been entered!'});
   };
 
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+
+    this.setState({message: 'Waiting on transaction success...'});
+
+    await lottery.methods.pickWinner().send({
+      from: accounts[0],
+    });
+
+    this.setState({message: 'A winner has been picked!'});
+  };
+
   render() {
     return (
         <div>
@@ -52,7 +64,12 @@ class App extends Component {
               <button>Enter</button>
             </div>
           </form>
+
           <hr/>
+          <h4>Ready to pick a winner?</h4>
+          <button onClick={this.onClick}>Pick a winner!</button>
+          <hr/>
+
           <h1>{this.state.message}</h1>
         </div>
     );
